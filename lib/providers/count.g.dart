bd4225163d80e51eb20ec139034a7ca8efa50937
AdnanNasr <zedxxxxx317@gmail.com>
2026-07-23 10:07:45 +0200

    new count provider

---
+++ lib/providers/count.g.dart
@@ -0,0 +1,61 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'count.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(Count)
+final countProvider = CountProvider._();
+
+final class CountProvider extends $NotifierProvider<Count, int> {
+  CountProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'countProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$countHash();
+
+  @$internal
+  @override
+  Count create() => Count();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(int value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<int>(value),
+    );
+  }
+}
+
+String _$countHash() => r'bc46bb51a5c2ba511d06cd76bbd5ab50b4ae6f03';
+
+abstract class _$Count extends $Notifier<int> {
+  int build();
+  @$mustCallSuper
+  @override
+  WhenComplete runBuild() {
+    final ref = this.ref as $Ref<int, int>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<int, int>,
+              int,
+              Object?,
+              Object?
+            >;
+    return element.handleCreate(ref, build);
+  }
+}
